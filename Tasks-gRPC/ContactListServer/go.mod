61822a9d0969c86a92774289ec46150e72372a8c
Jamshid7 <umarovjamshid8@gmail.com>
2021-05-29 12:36:50 +0500

    updated, added client&server relationship

---
+++ Tasks-gRPC/ContactListServer/go.mod
@@ -0,0 +1,10 @@
+module example.com/chat-server
+
+go 1.16
+
+require (
+	github.com/kisielk/gotool v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ Tasks-gRPC/ContactListServer/server.go
@@ -0,0 +1,26 @@
+package main
+import(
+	"log"
+	"net"
+	
+	"google.golang.org/grpc"
+	"example.com/chat-server/chat"	
+)
+
+func main() {	
+	lis, err:= net.Listen("tcp", ":9000")
+	
+	if err != nil{
+		log.Fatalf("failed to listen on port 9000: %v", err)
+	}
+	
+	s := chat.Server{}
+	
+	grpcServer := grpc.NewServer()
+	
+	chat.RegisterChatServiceServer(grpcServer, &s)
+	
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC server over port 9000: %v", err)
+	}
+}
